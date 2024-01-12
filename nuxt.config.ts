d233b2393770070e758347007e9a508cb35326a4
MRajczyk <mikolajrajczyk01@gmail.com>
2024-01-12 22:24:45 +0100

    poc sending mails code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    pages: true,
-    modules: ['@sidebase/nuxt-auth'],
-    // @ts-ignore
-    auth: {
-        provider: {
-            type: 'authjs'
-        }
-    }
-})
+  devtools: { enabled: true },
+  pages: true,
+  modules: ["@sidebase/nuxt-auth"],
+  // @ts-ignore
+  auth: {
+    provider: {
+      type: "authjs",
+    },
+  },
+});
--- server/api/profiles/index.post.ts
+++ server/api/profiles/index.post.ts
@@ -1,10 +1,30 @@
 import { PrismaClient } from "@prisma/client";
 import { hash } from "bcrypt";
 import { Role } from "@prisma/client";
+import { createTransport } from "nodemailer";
 
 export default eventHandler(async (event) => {
   const SALT_ROUNDS = 10;
   const prisma: PrismaClient = event.context.prisma;
+
+  const transporter = createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: process.env.GMAIL_APP_EMAIL,
+      pass: process.env.GMAIL_APP_PASSWORD,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "mikolajrajczyk01@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
 
   const body = await readBody(event);
   const createUser = await prisma.user.create({
